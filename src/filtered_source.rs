4d36c3d3ca2177c912a885ab2be06872d39092a0
Aron Granberg <aron.granberg@gmail.com>
2020-10-03 13:15:11 +0200

    Initial commit

---
+++ src/filtered_source.rs
@@ -0,0 +1,217 @@
+use rodio::{Sample, Sink, Source};
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+use rustfft::FFTplanner;
+use std::io::BufReader;
+use std::{collections::VecDeque, fs::File, sync::Arc, sync::Mutex, thread, time};
+use synthrs::{
+    filter::{cutoff_from_frequency, lowpass_filter},
+    sample,
+};
+use time::{Duration, Instant};
+
+/// Internal function that builds a `FilteredSource` object.
+pub fn dynamic_filter<I>(input: I) -> (FilteredSource<I>, Controller)
+where
+    I: Source<Item = f32>,
+{
+    let sample_rate = input.sample_rate();
+    let source = FilteredSource {
+        input,
+        settings: Arc::new(Mutex::new(Settings {
+            lowpass: vec![],
+            volume: 1.0,
+        })),
+        current_buffer: vec![],
+        current_buffer_index: 0,
+        input_buffer: vec![],
+        trailing_samples: vec![],
+    };
+
+    let controller = Controller {
+        sample_rate,
+        settings: source.settings.clone(),
+    };
+
+    controller.set_lowpass(5000.0);
+
+    (source, controller)
+}
+
+pub struct Settings {
+    lowpass: Vec<f32>,
+    volume: f32,
+}
+
+/// Filter that modifies reduces the volume to silence over a time period.
+#[derive(Clone)]
+pub struct FilteredSource<I> {
+    input: I,
+    input_buffer: Vec<f32>,
+    settings: Arc<Mutex<Settings>>,
+    trailing_samples: Vec<f32>,
+    current_buffer_index: usize,
+    current_buffer: Vec<f32>,
+}
+
+pub struct Controller {
+    sample_rate: u32,
+    settings: Arc<Mutex<Settings>>,
+}
+
+impl Controller {
+    pub fn set_lowpass(&self, freq: f64) {
+        let lowpass = lowpass_filter(
+            cutoff_from_frequency(
+                freq.min((self.sample_rate / 2) as f64),
+                self.sample_rate as usize,
+            ),
+            0.01,
+        );
+        self.settings.lock().unwrap().lowpass = lowpass.iter().map(|&x| x as f32).collect();
+    }
+
+    pub fn set_volume(&self, v: f64) {
+        self.settings.lock().unwrap().volume = v as f32;
+    }
+}
+
+pub fn convolve(filter: &[f32], input: &[f32], output: &mut [f32]) {
+    assert_eq!(output.len(), input.len() - filter.len(), "output size are only the inner valid samples. filter.len()/2 samples on each side are skipped.");
+    assert_eq!(filter.len() % 2, 0, "filter must have an even length");
+    assert!(
+        input.len() >= filter.len(),
+        "input must be at least as long as filter"
+    );
+
+    let h_len = (filter.len() / 2) as usize;
+    for i in h_len..input.len() - h_len {
+        let mut v = 0.0;
+        for j in 0..filter.len() {
+            v += input[i + j - h_len] * filter[j];
+        }
+        output[i - h_len] = v;
+    }
+}
+
+
+pub fn convolve_f64(filter: &[f64], input: &[f64], output: &mut [f64]) {
+    assert_eq!(output.len(), input.len() - filter.len(), "output size are only the inner valid samples. filter.len()/2 samples on each side are skipped.");
+    assert_eq!(filter.len() % 2, 0, "filter must have an even length");
+    assert!(
+        input.len() >= filter.len(),
+        "input must be at least as long as filter"
+    );
+
+    let h_len = (filter.len() / 2) as usize;
+    for i in h_len..input.len() - h_len {
+        let mut v = 0.0;
+        for j in 0..filter.len() {
+            v += input[i + j - h_len] * filter[j];
+        }
+        output[i - h_len] = v;
+    }
+}
+
+impl<I> Iterator for FilteredSource<I>
+where
+    I: Source<Item = f32>,
+    I::Item: Sample,
+{
+    type Item = f32;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_buffer_index < self.current_buffer.len() {
+            self.current_buffer_index += 1;
+            return Some(self.current_buffer[self.current_buffer_index - 1]);
+        }
+
+        {
+            let settings = self.settings.lock().unwrap();
+            let lowpass = &settings.lowpass;
+
+            // Must be at least the same size as the filter
+            let frame_size = 1024 - lowpass.len();
+
+            let input_samples = &mut self.input_buffer;
+            input_samples.clear();
+            input_samples.append(&mut self.trailing_samples);
+            input_samples.extend(
+                self.input
+                    .by_ref()
+                    .chain(std::iter::repeat(0.0))
+                    .take(frame_size),
+            );
+
+            assert!(
+                input_samples.len() >= lowpass.len(),
+                "{} >= {}",
+                input_samples.len(),
+                lowpass.len()
+            );
+
+            self.trailing_samples
+                .extend_from_slice(&input_samples[input_samples.len() - lowpass.len()..]);
+
+            let buffer = &mut self.current_buffer;
+            buffer.resize(input_samples.len() - lowpass.len(), 0.0);
+            let t1 = Instant::now();
+            convolve(&lowpass, &input_samples, buffer);
+            let t2 = Instant::now();
+
+            for s in buffer {
+                *s *= settings.volume;
+                *s = s.min(1.0).max(-1.0);
+            }
+
+            self.current_buffer_index = 0;
+        }
+
+        self.next()
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let inner = self.input.size_hint();
+        (
+            inner.0 + self.current_buffer.len(),
+            inner.1.map(|x| x + self.current_buffer.len()),
+        )
+    }
+}
+
+impl<I> ExactSizeIterator for FilteredSource<I>
+where
+    I: Source<Item = f32> + ExactSizeIterator,
+    I::Item: Sample,
+{
+}
+
+impl<I> Source for FilteredSource<I>
+where
+    I: Source<Item = f32>,
+    I::Item: Sample,
+{
+    #[inline]
+    fn current_frame_len(&self) -> Option<usize> {
+        self.input
+            .current_frame_len()
+            .map(|x| x + self.current_buffer.len())
+    }
+
+    #[inline]
+    fn channels(&self) -> u16 {
+        self.input.channels()
+    }
+
+    #[inline]
+    fn sample_rate(&self) -> u32 {
+        self.input.sample_rate()
+    }
+
+    #[inline]
+    fn total_duration(&self) -> Option<Duration> {
+        self.input.total_duration()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,242 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+use rocket::State;
+use rocket_contrib::json::Json;
+use rodio::{source::Done, Sample, Sink, Source};
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+use rustfft::FFTplanner;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::VecDeque, ffi::OsStr, ffi::OsString, fs::File, sync::Arc, sync::Mutex, thread,
+    time,
+};
+use std::{io::BufReader, path::Path, path::PathBuf, sync::atomic::AtomicUsize};
+use synthrs::{
+    filter::{cutoff_from_frequency, lowpass_filter},
+    sample,
+};
+use time::{Duration, Instant};
+mod filtered_source;
+use chrono::{DateTime, Utc};
+use filtered_source::dynamic_filter;
+use rand::prelude::*;
+use thiserror::Error;
+
+#[macro_use]
+extern crate rocket;
+
+fn frquency_cutoff_lp(t: f32) -> f32 {
+    let clamped_t = (t - 10.0).max(0.0);
+    100000.0f32.min(800.0 + clamped_t.powf(2.5) * 1.0)
+}
+
+fn reverb_balance(t: f32) -> f32 {
+    0.5 / (t * 0.03 + 1.0)
+}
+
+fn volume(t: f32) -> f32 {
+    return 1.0f32.min(0.0 + 0.007 * t + 0.0f32.max(t - 5.0) * 0.013);
+}
+
+fn smoothstep(x: f32) -> f32 {
+    3.0 * x.powi(2) - 2.0 * x.powi(3)
+}
+
+fn fadeout(t: f32, duration: f32) -> f32 {
+    smoothstep((1.0 - (t / duration)).max(0.0))
+}
+
+#[derive(Clone)]
+struct AlarmState {
+    inner: Arc<Mutex<InnerAlarmState>>,
+}
+
+impl AlarmState {
+    fn should_start_alarm(&self) -> bool {
+        let state = self.inner.lock().unwrap();
+        state.enabled && Utc::now() >= state.next_alarm
+    }
+
+    fn disable(&self) {
+        let mut state = self.inner.lock().unwrap();
+        state.enabled = false;
+    }
+}
+struct InnerAlarmState {
+    next_alarm: DateTime<Utc>,
+    enabled: bool,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct AlarmInfo {
+    time: String,
+    enabled: bool,
+}
+
+#[get("/get")]
+fn get_info(state: State<AlarmState>) -> Json<AlarmInfo> {
+    let state = state.inner.lock().unwrap();
+    Json(AlarmInfo {
+        time: state.next_alarm.to_rfc3339(),
+        enabled: state.enabled,
+    })
+}
+
+#[post("/store", data = "<info>")]
+fn store(info: Json<AlarmInfo>, state: State<AlarmState>) {
+    let mut state = state.inner.lock().unwrap();
+    state.next_alarm = DateTime::parse_from_rfc3339(&info.time)
+        .expect("Could not parse date")
+        .into();
+    println!(
+        "Set alarm to {} which is {} minutes into the future",
+        state.next_alarm,
+        state
+            .next_alarm
+            .signed_duration_since(Utc::now())
+            .num_minutes()
+    );
+    state.enabled = info.enabled;
+}
+
+fn start_server(alarm_state: AlarmState) {
+    rocket::ignite()
+        .manage(alarm_state)
+        .mount("/", routes![get_info, store])
+        .launch();
+}
+
+fn play(path: &PathBuf, alarm_state: &AlarmState) {
+    let (stream, handle) = rodio::OutputStream::try_default().unwrap();
+
+    let sink = Sink::try_new(&handle).unwrap();
+
+    // Add a dummy source of the sake of the example.
+    let file = File::open(path).unwrap();
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+
+    let (source, controller) = dynamic_filter(source.convert_samples());
+    sink.append(source);
+
+    let alarm_timeout = 120.0;
+
+    let t0 = Instant::now();
+    loop {
+        let t = Instant::now().duration_since(t0).as_secs_f32();
+        if t > alarm_timeout || !alarm_state.should_start_alarm() {
+            break;
+        }
+
+        let freq = frquency_cutoff_lp(t);
+        controller.set_lowpass(freq as f64);
+        controller.set_volume(volume(t) as f64);
+        thread::sleep(Duration::from_millis(40));
+    }
+
+    let t1 = Instant::now();
+    let fadeout_duration = 10.0;
+    loop {
+        let t = Instant::now().duration_since(t0).as_secs_f32();
+        let t_fadeout = Instant::now().duration_since(t1).as_secs_f32();
+        if t_fadeout >= fadeout_duration {
+            break;
+        }
+        let freq = frquency_cutoff_lp(t);
+        controller.set_volume(volume(t) as f64 * fadeout(t_fadeout, fadeout_duration) as f64);
+        controller.set_lowpass(freq as f64);
+    }
+
+    controller.set_volume(0.0);
+    sink.stop();
+    alarm_state.disable();
+}
+
+#[derive(Error, Debug)]
+enum AlarmSoundError {
+    #[error("Could not read directory `{0}`: {1}")]
+    CouldNotReadDir(PathBuf, std::io::Error),
+    #[error("There were no sound files in the sound directory")]
+    NoFiles,
+}
+
+fn random_alarm_sound(root_dir: &Path) -> Result<PathBuf, AlarmSoundError> {
+    let valid_extensions = ["mp3", "ogg", "flac", "wav"];
+    match root_dir.read_dir() {
+        Ok(iter) => iter
+            .filter_map(|x| x.ok().map(|x| x.path()))
+            .filter(|path| {
+                path.extension()
+                    .and_then(OsStr::to_str)
+                    .map(|x| valid_extensions.contains(&x))
+                    .unwrap_or_default()
+            })
+            .choose(&mut rand::thread_rng())
+            .ok_or(AlarmSoundError::NoFiles),
+        Err(e) => Err(AlarmSoundError::CouldNotReadDir(root_dir.to_path_buf(), e)),
+    }
+}
+
+fn start_alarm_thread(alarm_state: AlarmState) {
+    loop {
+        if alarm_state.should_start_alarm() {
+            println!("Starting alarm...");
+            match random_alarm_sound(Path::new("./sounds")) {
+                Ok(path) => {
+                    println!("Playing {}", path.to_str().unwrap());
+                    play(&path, &alarm_state)
+                },
+                Err(e) => {
+                    println!("{}", e);
+                    alarm_state.disable();
+                }
+            }
+            println!("Alarm finished...");
+        }
+
+        thread::sleep(Duration::from_millis(500));
+    }
+}
+
+fn main() {
+    let alarm_state = AlarmState {
+        inner: Arc::new(Mutex::new(InnerAlarmState {
+            next_alarm: Utc::now(),
+            enabled: false,
+        })),
+    };
+
+    let audio_alarm_state = alarm_state.clone();
+    thread::spawn(move || start_alarm_thread(audio_alarm_state));
+    start_server(alarm_state.clone());
+}
+
+// use synthrs::synthesizer::{ make_samples, quantize_samples };
+// use synthrs::writer::write_wav_file;
+
+// fn main() {
+//     let file = File::open("test.wav").unwrap();
+//     let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+//     let sample_rate = source.sample_rate() as usize;
+//     let samples = source
+//     .convert_samples::<f32>().step_by(2).map(|x| x as f64).collect::<Vec<f64>>();
+//     println!("Done reading");
+
+//     let lowpass = lowpass_filter(cutoff_from_frequency(5000.0, sample_rate), 0.01);
+//     // let samples = synthrs::filter::convolve(&lowpass, &samples);
+//     let mut output_samples = vec![];
+//     output_samples.resize(samples.len() - lowpass.len(), 0.0f64);
+//     convolve_f64(&lowpass, &samples, &mut output_samples);
+//     let samples = output_samples;
+
+//     // let (samples, samples_len) =
+//     //     sample::samples_from_wave_file("./test.wav").unwrap();
+
+//     let quantized = quantize_samples::<i16>(
+//         &samples
+//     );
+//     println!("Writing");
+//     // Using a predefined generator
+//     write_wav_file("out.wav", sample_rate,
+//         &quantized
+//     ).expect("failed to write to file");
+// }
