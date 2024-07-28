3d9cbe1612662fcc09a7061ba194a430aae5b694
Aron Granberg <aron.granberg@gmail.com>
2024-07-28 21:18:53 +0200

    Code cleanup.

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,10 @@ pub struct AlarmState {
     last_played: Arc<SyncedContainer<LastPlayed>>,
     #[cfg(feature = "motion")]
     sleep_monitor: Arc<Mutex<SleepMonitorState>>,
+    #[allow(dead_code)]
     storage: SyncStorage,
     is_playing: Arc<SyncedContainer<bool>>,
+    #[allow(dead_code)]
     is_user_in_bed: Arc<SyncedContainer<bool>>,
 }
 
@@ -274,12 +276,6 @@ async fn main() -> Result<(), rocket::Error> {
         }
     };
 
-    // let remote_server = if std::env::args().any(|x| x == "--remote-sync") {
-    //     Some("http://alarm.arongranberg.com")
-    // } else {
-    //     None
-    // };
-
     let machine_id = machineid_rs::IdBuilder::new(machineid_rs::Encryption::SHA256)
         .add_component(HWIDComponent::SystemID)
         .build("somekey")
@@ -292,7 +288,7 @@ async fn main() -> Result<(), rocket::Error> {
         MQTT_PASSWORD,
     )
     .await;
-    println!("0");
+
     let inner_state = storage
         .add_container(
             "alarm/state",
@@ -345,21 +341,7 @@ async fn main() -> Result<(), rocket::Error> {
             ),
             alarm_is_playing: false,
         })),
-        // sync_url: remote_server,
     };
-
-    let r = rocket::build().manage(alarm_state.clone()).mount(
-        "/",
-        routes![
-            get_info,
-            get_info_compat,
-            store_compat,
-            // on_alarm_finished,
-            get_state,
-            put_state
-        ],
-    );
-    let rocket_task = tokio::spawn(r.launch());
 
     #[cfg(feature = "motion")]
     {
@@ -367,12 +349,8 @@ async fn main() -> Result<(), rocket::Error> {
         thread::spawn(move || monitor_sleep(sm));
     }
 
-    // if let Some(url) = remote_server {
-    //     sync_down(&alarm_state, url).unwrap();
-    // }
-
     if play_immediately {
-        println!("Playing alarm immediately");
+        info!("Playing alarm immediately");
         alarm_state
             .inner
             .update(|s| {
@@ -382,51 +360,27 @@ async fn main() -> Result<(), rocket::Error> {
             .await;
     }
 
-    // if let Some(url) = remote_server {
-    //     sync_up(&alarm_state).unwrap();
-    //     let audio_alarm_state2 = alarm_state.clone();
-    //     thread::spawn(move || start_sync_thread(audio_alarm_state2, url));
-    // }
-
     #[cfg(feature = "audio")]
     {
         let audio_alarm_state = alarm_state.clone();
         thread::spawn(move || alarm::start_alarm_thread(audio_alarm_state));
     }
 
-    println!("Rocket??");
-    rocket_task.await.unwrap().unwrap();
-    println!("Rocket!");
+    rocket::build()
+        .manage(alarm_state.clone())
+        .mount(
+            "/",
+            routes![
+                get_info,
+                get_info_compat,
+                store_compat,
+                get_state,
+                put_state
+            ],
+        )
+        .launch()
+        .await
+        .unwrap();
+
     Ok(())
 }
-
-// use synthrs::synthesizer::{ make_samples, quantize_samples };
-// use synthrs::writer::write_wav_file;
-
-// fn main() {
-//     let file = File::open("test.wav").unwrap();
-//     let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-//     let sample_rate = source.sample_rate() as usize;
-//     let samples = source
-//     .convert_samples::<f32>().step_by(2).map(|x| x as f64).collect::<Vec<f64>>();
-//     println!("Done reading");
-
-//     let lowpass = lowpass_filter(cutoff_from_frequency(5000.0, sample_rate), 0.01);
-//     // let samples = synthrs::filter::convolve(&lowpass, &samples);
-//     let mut output_samples = vec![];
-//     output_samples.resize(samples.len() - lowpass.len(), 0.0f64);
-//     convolve_f64(&lowpass, &samples, &mut output_samples);
-//     let samples = output_samples;
-
-//     // let (samples, samples_len) =
-//     //     sample::samples_from_wave_file("./test.wav").unwrap();
-
-//     let quantized = quantize_samples::<i16>(
-//         &samples
-//     );
-//     println!("Writing");
-//     // Using a predefined generator
-//     write_wav_file("out.wav", sample_rate,
-//         &quantized
-//     ).expect("failed to write to file");
-// }
